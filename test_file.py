a8baec0db33e4c91f685c3fb96eb469b0bd4cb71
Ghanashyam31 <Ghanashyam.patole17@gmail.com>
2026-07-07 19:28:19 +0530

    loop added

--- test_file.py
+++ test_file.py
@@ -5,3 +5,7 @@ dttime=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 print(f"Current date and time: {dttime}")
 
 print("line changed. Now added further comments to test the git commit and push functionality.")
+
+
+for i in range(10):
+    print(f"Iteration {i+1}: Current date and time: {dttime}")
